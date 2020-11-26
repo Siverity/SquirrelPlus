8c38903838984fff29abb97b351c3024c93fc934
TheBoogle <theboogledoo@gmail.com>
2020-11-25 22:37:16 -0800

    Create punish.lua

---
+++ Modules/punish.lua
@@ -0,0 +1,32 @@
+-- Written by Boogagle(Boogle#4509)
+-- 11/25/2020
+
+local Main = {}
+local MT = {}; MT.__index = Main
+
+local self = {}
+
+-- Services
+local DSS = game:GetService("DataStoreService")
+
+
+
+function Main.new(DatastoreName, KeyFormat)
+
+	if not DatastoreName then warn("Please pass a DatastoreName when calling Module.new()") return end
+
+	if not KeyFormat then warn("Please pass a KeyFormat when calling Module.new()") return end
+
+	self.DS = DSS:GetDataStore(DatastoreName)
+
+
+
+	return setmetatable(self, MT)
+end
+
+
+function Main:LookUp(Player)
+	local SyncedData = self.DS:GetAsync((self.KeyFormat):format(Player.UserId))
+
+	return SyncedData
+end
